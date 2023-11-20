a4314a18e006b7709b713cf37200ffd02538884d
dbjelcevic <dominik.bjelcevic@barrage.net>
2023-11-20 09:29:25 +0100

    Move code from config to impl class

--- src/main/java/net/barrage/school/java/ecatalog/app/ProductServiceImpl.java
+++ src/main/java/net/barrage/school/java/ecatalog/app/ProductServiceImpl.java
@@ -3,7 +3,9 @@ package net.barrage.school.java.ecatalog.app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import net.barrage.school.java.ecatalog.model.Product;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -48,5 +50,17 @@ public class ProductServiceImpl implements ProductService {
                 .filter(p -> Objects.requireNonNullElse(p.getName(), "").toLowerCase().contains(q) || Objects.requireNonNullElse(p.getDescription(), "").toLowerCase().contains(q))
                 .toList();
     }
-    
+
+
+    @Scheduled(fixedRate = 5000)
+    @CacheEvict(value = "products", allEntries = true)
+    public void clearProductCache() {
+    }
+
+    @Scheduled(fixedRate = 10000)
+    @CacheEvict(value = "search", allEntries = true)
+    public void clearSearchCache() {
+    }
+
+
 }
